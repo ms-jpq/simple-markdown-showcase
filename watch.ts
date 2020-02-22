d7801bc603095831ea1f080cb1843269702c0bd3
Fly me to the moon <git@bigly.dog>
2020-02-21 19:20:11 -0800

    use proper gitignore

--- watch.ts
+++ watch.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env ts-node
+import ignore from "ignore"
 import { ChildProcess, spawn } from "child_process"
+import { promises as fs } from "fs"
+import { relative } from "path"
 import { watchTree } from "watch"
 
 const run = (program: string, ...args: string[]) => {
@@ -30,9 +33,11 @@ const thunk = (fn: () => void, ms: number) => {
   }
 }
 
-const watch = (run: () => void, timeout: number) => {
-  const ignoreDirectoryPattern = /node_modules|out|temp/g
-  watchTree(__dirname, { ignoreDirectoryPattern }, thunk(run, timeout))
+const watch = async (run: () => void, timeout: number) => {
+  const git_ignore = (await fs.readFile(".gitignore")).toString()
+  const ig = ignore().add(git_ignore)
+  const filter = (file: string) => ig.ignores(relative(__dirname, file))
+  watchTree(__dirname, { filter }, thunk(run, timeout))
 }
 
 watch(run("src/entry.ts"), 100)
