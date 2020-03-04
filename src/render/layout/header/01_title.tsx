eb58a13f9b65d11defe2b22bd5d1d25c12f310e5
Fly me to the moon <git@bigly.dog>
2020-03-03 19:38:08 -0800

    improve layout

--- src/render/layout/header/01_title.tsx
+++ src/render/layout/header/01_title.tsx
@@ -4,9 +4,9 @@ import { cn } from "../../../domain_agnostic/isomorphic/dom"
 export type TitleProps = { title: string }
 
 export const HeaderTitle = ({ title }: TitleProps) => (
-  <h3 className={cn("text-uppercase", "text-right")}>
+  <h2 className={cn("text-uppercase", "text-right", "text-ellipsis")}>
     <a href="/" className={cn("invis-link")}>
       {title}
     </a>
-  </h3>
+  </h2>
 )
