3298d2c0d4f64267242823c1e0f623a26682253a
Fly me to the moon <git@bigly.dog>
2020-03-07 15:43:02 -0800

    margin updates

--- src/render/layout/header/01_title.tsx
+++ src/render/layout/header/01_title.tsx
@@ -4,7 +4,10 @@ import { cn } from "nda/dist/isomorphic/dom"
 export type TitleProps = { title: string }
 
 export const HeaderTitle = ({ title }: TitleProps) => (
-  <h2 id="header-title" className={cn("text-uppercase", "text-ellipsis")}>
+  <h2
+    id="header-title"
+    className={cn("text-uppercase", "text-ellipsis", "mb-0")}
+  >
     <a href="/" className={cn("invis-link")}>
       {title}
     </a>
