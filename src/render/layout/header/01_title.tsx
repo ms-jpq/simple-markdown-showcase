9a9ad27a024514f4efbf224c34d67301bf3bf5f2
Fly me to the moon <git@bigly.dog>
2020-03-07 14:58:15 -0800

    fixed text align for title

--- src/render/layout/header/01_title.tsx
+++ src/render/layout/header/01_title.tsx
@@ -4,7 +4,7 @@ import { cn } from "nda/dist/isomorphic/dom"
 export type TitleProps = { title: string }
 
 export const HeaderTitle = ({ title }: TitleProps) => (
-  <h2 className={cn("text-uppercase", "text-right", "text-ellipsis")}>
+  <h2 id="header-title" className={cn("text-uppercase", "text-ellipsis")}>
     <a href="/" className={cn("invis-link")}>
       {title}
     </a>
