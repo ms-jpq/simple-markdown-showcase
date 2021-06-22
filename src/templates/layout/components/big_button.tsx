f026a2938d36ed0017f384379b9a0852a2f21851
I love my dog <git@bigly.dog>
2021-06-22 04:11:45 -0700

    mv

--- src/templates/layout/components/big_button.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { cn } from "nda/dist/isomorphic/dom"
-import { Parent } from "../../../vender/react"
-
-export type ButtonProps = {} & Parent
-export const BigButton = ({ children }: ButtonProps) => (
-  <button className={cn("big-button", "text-justify")}>{children}</button>
-)
