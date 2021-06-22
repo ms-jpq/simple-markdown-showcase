d4e337be18a70d660bd83c4a72c9462f2196d63f
I love my dog <git@bigly.dog>
2021-06-22 03:54:28 -0700

    oh god

--- src/render/layout/footer/00_desc.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { cn } from "nda/dist/isomorphic/dom"
-import { Markdown } from "../markdown"
-
-export type DescProps = { md_line: string }
-export const FooterDesc = ({ md_line }: DescProps) => (
-  <Markdown content={md_line} />
-)
