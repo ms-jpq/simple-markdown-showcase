1fcb2ec2e2bed7d77fc506852c92173bc20f3119
I love my dog <git@bigly.dog>
2021-06-22 15:05:54 -0700

    ignore

--- src/js/layout.ts
+++ src/js/layout.ts
@@ -1,6 +1,5 @@
-import { $ } from "nda/dist/browser/dom"
-
-const header_menu = $<HTMLButtonElement>(`header > button`)!
+const header_menu = document.querySelector(`header > button`)!
 header_menu.addEventListener("click", () =>
-  document.body.classList.toggle("col2"),
+  document.body.classList.toggle("col2")
 )
+
