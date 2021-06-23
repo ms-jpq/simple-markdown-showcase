cf5d0f061b45552325ae8a6220c7eb0587af9268
I love my dog <git@bigly.dog>
2021-06-23 04:25:51 -0700

    404 wot

--- src/optimize.py
+++ src/optimize.py
@@ -1,10 +1,51 @@
-from pathlib import Path
+from asyncio.tasks import gather
+from hashlib import sha256
+from pathlib import Path, PosixPath
+from typing import Awaitable, Iterator
+from urllib.parse import urlsplit
 
+from std2.asyncio import run_in_executor
+from std2.urllib import urlopen
+
+from .consts import CACHE_DIR, TIMEOUT
 from .log import log
 from .parse import Node, ParseError, parse
 
+_IMG_CACHE = CACHE_DIR / "img"
 
-def optimize(path: Path, html: str) -> str:
+
+async def _fetch(uri: str) -> bytes:
+    def cont() -> bytes:
+        with urlopen(uri, timeout=TIMEOUT) as resp:
+            buf = resp.read()
+        return buf
+
+    return await run_in_executor(cont)
+
+
+async def _localize(node: Node) -> None:
+    assert node.tag == "img"
+    src = node.attrs.get("src")
+    if src:
+        ext = PosixPath(urlsplit(src).path).suffix
+        sha = sha256(src.encode()).hexdigest()
+        name = Path(sha).with_suffix(ext)
+        path = _IMG_CACHE / name
+
+        if not path.exists():
+            buf = await _fetch(src)
+            path.write_bytes(buf)
+
+
+def _optimize(node: Node) -> Iterator[Awaitable[None]]:
+    _IMG_CACHE.mkdir(parents=True, exist_ok=True)
+    for n in node:
+        if isinstance(n, Node):
+            if n.tag == "img":
+                yield _localize(n)
+
+
+async def optimize(path: Path, html: str) -> str:
     try:
         node = parse(html)
     except ParseError:
@@ -16,5 +57,6 @@ def optimize(path: Path, html: str) -> str:
             for child in node.children
             if isinstance(child, Node) and child.tag == "html"
         )
+        await gather(*_optimize(main))
         return "<!DOCTYPE html>" + str(main)
 
