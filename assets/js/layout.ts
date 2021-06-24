edd3d4990a0c081be06157478fdd599973ac1304
I love my dog <git@bigly.dog>
2021-06-24 07:12:11 -0700

    cool beans

--- assets/js/layout.ts
+++ assets/js/layout.ts
@@ -1,4 +1,12 @@
-const header_menu = document.querySelector(`header > button`)!;
+import { $, $$ } from "nda/dist/browser/dom";
+
+const header_menu = $<HTMLButtonElement>(`header > button`)!;
+const times = $$<HTMLTimeElement>(`time`)!;
+
+for (const el of times) {
+  el.innerText = new Date(el.dateTime).toLocaleString();
+}
+
 header_menu.addEventListener("click", () =>
   document.body.classList.toggle("col2")
 );
