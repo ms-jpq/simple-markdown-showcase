3cf5093db2af20cd3e87a01c2c80e7c5892153c9
I love my dog <git@bigly.dog>
2021-06-24 16:21:04 -0700

    build

--- src/optimize.py
+++ src/optimize.py
@@ -3,7 +3,8 @@ from asyncio.tasks import gather
 from functools import lru_cache
 from hashlib import sha256
 from http import HTTPStatus
-from pathlib import Path, PurePosixPath
+from os import sep
+from pathlib import Path, PurePath, PurePosixPath
 from shutil import copy2
 from typing import Awaitable, Iterator
 from urllib.error import HTTPError
@@ -12,7 +13,7 @@ from urllib.parse import SplitResult, urlsplit, urlunsplit
 from std2.asyncio import run_in_executor
 from std2.urllib import urlopen
 
-from .consts import ASSETS,  TIMEOUT ,DIST_DIR
+from .consts import ASSETS, DIST_DIR, TIMEOUT
 from .log import log
 from .parse import Node, ParseError, parse
 
@@ -26,41 +27,43 @@ def _get(uri: str) -> bytes:
 
 
 @lru_cache
-def _fetch(uri: SplitResult, cache_path: Path) -> Awaitable[None]:
+def _fetch(uri: SplitResult, path: Path) -> Awaitable[None]:
     def cont() -> None:
-        if not cache_path.exists():
+        if not path.exists():
             if uri.scheme in {"http", "https"}:
                 src = urlunsplit(uri)
                 try:
                     buf = _get(src)
-                    cache_path.write_bytes(buf)
+                    path.write_bytes(buf)
                 except HTTPError as e:
                     if e.code in {HTTPStatus.FORBIDDEN}:
                         pass
                     else:
                         log.exception("%s", f"{e} -- {src}")
                 else:
-                    cache_path.write_bytes(buf)
+                    path.write_bytes(buf)
             else:
                 img_path = _IMG_DIR / uri.path
                 if img_path.is_file():
-                    copy2(img_path, cache_path)
+                    copy2(img_path, path)
 
     return create_task(run_in_executor(cont))
 
 
 async def _localize(node: Node) -> None:
     assert node.tag == "img"
-    src = node.attrs.get("src") or ""
-    uri = urlsplit(src)
+    src = node.attrs.get("src")
+    if not src:
+        raise KeyError(str(node))
+    else:
+        uri = urlsplit(src)
+        ext = PurePosixPath(uri.path).suffix.casefold()
+        hashed_path = PurePath(sha256(src.encode()).hexdigest()).with_suffix(ext)
+        new_path = DIST_DIR / hashed_path
+        await _fetch(uri, path=new_path)
 
-    ext = PurePosixPath(uri.path).suffix.casefold()
-    sha = sha256(src.encode()).hexdigest()
-    new_path = (DIST_DIR / sha).with_suffix(ext)
-    await _fetch(uri, cache_path=new_path)
-
-    node.attrs["loading"] = "lazy"
-    node.attrs["src"] = ""
+        node.attrs["loading"] = "lazy"
+        node.attrs["src"] = str(PurePosixPath(sep) / hashed_path)
 
 
 def _optimize(node: Node) -> Iterator[Awaitable[None]]:
