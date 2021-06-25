d056a29c52c197f567c9f5c4f1d5d67a621dac46
I love my dog <git@bigly.dog>
2021-06-24 21:46:29 -0700

    just

--- src/timeit.py
+++ src/timeit.py
@@ -2,8 +2,8 @@ from contextlib import contextmanager
 from shutil import get_terminal_size
 from typing import Any, Iterator
 
-from std2.timeit import timeit as _timeit
 from std2.locale import si_prefixed_smol
+from std2.timeit import timeit as _timeit
 
 from .log import log
 
@@ -13,8 +13,8 @@ def timeit(label: str, *args: Any) -> Iterator[None]:
     with _timeit() as t:
         yield None
     delta = t()
-    time = si_prefixed_smol(delta)
+    time = f"{si_prefixed_smol(delta)}s".ljust(8)
     cols, _ = get_terminal_size()
-    msg = f"{label.ljust(10)} -- {time}s  " + " ".join(map(str, args)) + " "
+    msg = f"{label.ljust(10)} -- {time}  " + " ".join(map(str, args)) + " "
     log.info("%s", msg.ljust(cols, "="))
 
